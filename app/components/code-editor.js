58ae273dda9d752b6af9c1ec359bf06cd9b88d4f
Derek Cochran <derek@skylark95.com>
2018-04-27 19:12:03 -0500

    Save button

--- app/components/code-editor.js
+++ app/components/code-editor.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  valueUpdated: function() {},
+  actions: {
+    updateSourceText(sourceText) {
+      this.set('sourceText', sourceText);
+      this.get('valueUpdated')();
+    }
+  }
 });
--- app/controllers/fiddle.js
+++ app/controllers/fiddle.js
@@ -1,9 +1,21 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  alertInfo: '',
+  saveDisabled: false,
   actions: {
     saveFiddle() {
-      this.get('model').save();
+      this.set('saveDisabled', true);
+      this.set('alertInfo', 'Saving Fiddle...');
+      this.get('model').save().then(() => {
+        this.set('alertInfo', 'Fiddle Saved.');
+        setTimeout(() => {
+          this.set('alertInfo', '');
+        }, 4000)
+      });
+    },
+    enableSave() {
+      this.set('saveDisabled', false);
     }
   }
 });
